d9a0a661caf72389f1dfa90319c6b3910ad76a4d
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2024-08-30 17:57:00 -0600

    Finished Troubleshooting Deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,12 +2,6 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 let sequelize;
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("process.env:", process.env);
 
 sequelize = new Sequelize(
   process.env.DB_NAME,
