97e18e273cd113a7c359e88442988b8fe387c8b2
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2024-08-30 17:23:59 -0600

    More Troubleshooting Deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,6 +7,7 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
+console.log("process.env:", process.env);
 
 sequelize = new Sequelize(
   process.env.DB_NAME,
